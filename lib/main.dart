5c1d9d96b644a8086c84396e4eb61c6744814888
Omar Shafei <omarshafhy.os@gmail.com>
2019-12-09 09:19:56 +0200

    The final version with new icon

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I Am Rich"),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
